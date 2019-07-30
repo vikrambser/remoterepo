41da72c10e37b5c559024b9b263b16d006b19a55
vikram <vikrambser@gmail.com>
2019-07-30 11:55:48 +0530

    calc

---
+++ javaprjct/src/javaprjct/calc.java
@@ -0,0 +1,53 @@
+package javaprjct;
+
+import java.util.*;
+
+
+
+
+
+public class calc {
+	
+	public static int Add(int x , int y)
+	{
+	  return x+y;	
+	  
+	}
+
+	public static int Mul(int x, int y)
+	{
+	  return x*y;	
+	}
+
+	public static int Sub(int x, int y)
+	{
+	  return x-y;	
+	}
+
+	public static int Div(int x, int y)
+	{
+	  return x/y;	
+	}
+	
+	
+	
+	public static void main(String[] args) {
+	
+	int x=5,y=2;
+	Scanner In =  new Scanner(System.in);
+	System.out.println("Addition");
+	int A = Add(x,y);
+	System.out.println(A);
+	System.out.println("Substraction");
+	int S = Sub(x,y);
+	System.out.println(S);
+	System.out.println("Multiplication");
+	int M = Mul(x,y);
+	System.out.println(M);
+	System.out.println("Division");
+	int D = Div(x,y);
+	System.out.println(D);
+	
+	}
+
+}
